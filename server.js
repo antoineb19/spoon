beb7f4d7a9745002f3870fcbd31240de522d2a09
antoineb19 <antoinebenoit@msn.com>
2016-04-05 21:13:45 +0200

    initial server.js commit

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Vous êtes à l\'accueil');
+});
+
+app.listen(8080);
+console.log("Server launched !");
