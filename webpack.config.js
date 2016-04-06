8d11122f5d9573da7824ea31758d317b22cc5924
antoineb19 <antoinebenoit@msn.com>
2016-04-06 22:34:06 +0200

    test webpack

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const webpack =  require('webpack');
+const path = require('path');
+
+const PATHS = {
+    testSource: path.join(__dirname, "src/test-webpack/testSource"), 
+    testApp: path.join(__dirname, "src/test-webpack/testApp")
+};
+
+module.exports = {
+  entry: {
+    testApp: PATHS.testSource
+  },
+  output: {
+    path: PATHS.testApp,
+    filename: 'bundle.js'
+  }
+};
